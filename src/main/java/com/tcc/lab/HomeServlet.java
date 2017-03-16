88821a060f917c107dcf92d8ed822110534aeb60
Lance Johansson <lcheng@campuscruiser.com>
2017-03-16 16:08:30 +0800

    maven package

--- src/main/java/com/tcc/lab/HomeServlet.java
+++
@@ -1,9 +0,0 @@
-package com.tcc.lab;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-
-@WebServlet(urlPatterns={"/hello"})
-public class HomeServlet extends HttpServlet {
-	
-}
