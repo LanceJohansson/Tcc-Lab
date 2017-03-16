6a49927f93c9da359511962a037a6ecee905d5cd
Lance Johansson <lcheng@campuscruiser.com>
2017-03-16 14:35:47 +0800

    Maven setting add java servlet

---
+++ src/main/java/com/tcc/lab/HomeServlet.java
@@ -0,0 +1,9 @@
+package com.tcc.lab;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns={"/hello"})
+public class HomeServlet extends HttpServlet {
+	
+}
